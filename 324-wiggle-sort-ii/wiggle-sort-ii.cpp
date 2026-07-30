6098fbbca8d03e88887548cc3932b5d3dc469c8d
farhan-web404 <kaladiyafarhan84@gmail.com>
2026-07-30 19:36:30 +0530

    Time: 3 ms (47.40%) | Memory: 22 MB (20.91%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        vector<int> v = nums;
+        sort(v.begin(), v.end());
+
+        int n = nums.size();
+        int j = (n - 1) / 2;
+        int k = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0)
+                nums[i] = v[j--];
+            else
+                nums[i] = v[k--];
+        }
+    }
+};
